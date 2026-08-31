7db88702f9afdf876dd6508e79198d52d2e201ff
Lemonade8080 <eclipse8031@gmail.com>
2026-08-31 19:49:16 +0900

    Add TimeManager

---
+++ Project1/TimeManager.cpp
@@ -0,0 +1,46 @@
+class TimeManager
+{
+public:
+	void TimeStart() {
+		if isRunning == false
+			isRunning = true;
+	}
+
+	void TimeUpdate(float deltatime) {
+		if isRunning == true{
+			currentTime += deltatime;
+		}
+	}
+	
+	void TimeReset() {
+		isRunning = false;
+		currentTime = 0.0f;
+	}
+
+	void TimePause() {
+		if isRunning == true{
+			isRunning = false;
+		}
+	}
+
+	void TimeResume() {
+		if isRunning == false{
+			isRunning = true;
+		}
+	}
+
+	float GetCurrentTime() {
+		return currentTime;
+	}
+
+	bool IsRunning() {
+		return isRunning;
+	}
+
+private :
+
+	float currentTime = 0.0f;
+	bool isRunning = false;
+
+
+};
