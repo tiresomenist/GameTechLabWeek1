0abb3f3b660610dd252b856d338c806505f19d93
Lemonade8080 <eclipse8031@gmail.com>
2026-09-01 10:23:29 +0900

    Add time-manager-h

--- Project1/TimeManager.cpp
+++ Project1/TimeManager.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include "TimeManager.h"
 
 class TimeManager
 {
---
+++ Project1/TimeManager.h
@@ -0,0 +1,16 @@
+class TimeManager
+{
+private:
+	float currentTime = 0.0f;
+	bool isRunning = false;
+
+public:
+	TimeManager() {};
+	void TimeStart() {}
+	void TimeUpdate(float deltatime) {}
+	void TimeReset() {}
+	void TimePause() {}
+	void TimeResume() {}
+	float GetCurrentTime() {}
+	bool IsRunning() {}
+};
