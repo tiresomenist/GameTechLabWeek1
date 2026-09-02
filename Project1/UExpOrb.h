c26017a35a9d839bfbee2d5e96d790a9b08c8cbb
tiresomenist <tiresomenist@gmail.com>
2026-09-02 19:11:07 +0900

    경험치도 회전

--- Project1/UExpOrb.h
+++ Project1/UExpOrb.h
@@ -4,13 +4,28 @@
 class UExpOrb : public UItem
 {
 	int giveExp = 10;
-
+	int dir = 1;
+	int limit = 3.141592f / 2.2f;
 	public:
 	UExpOrb();
 	~UExpOrb() = default;
 
 	int GetGiveExp() { return giveExp; }
 
+	void UpdateState() override
+	{
+		
+		//rotation.x += 0.05f;
+		rotation.y += 0.03f * dir;
+		if (rotation.y >= limit)
+		{
+			dir = -1;
+		}
+		else if (rotation.y <= -limit)
+		{
+			dir = 1;
+		}
+	}
 
 
 };
